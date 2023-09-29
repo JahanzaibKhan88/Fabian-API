a638151e9d3a670a115370ac3cd4e6db717218b5
JZKhan <jahan.zaib@live.com>
2023-09-29 23:24:27 +0500

    fixed missing references though they are not required

---
+++ Fabian.Infrastructure/Models/BrightBuckleContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fabian.Infrastructure.Models;
+
+public partial class FabianContext : DbContext
+{
+    public FabianContext()
+    {
+    }
+
+    public FabianContext(DbContextOptions<FabianContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=connectionStrings:DefaultConnectionString");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Customer__1788CC4C5F9A6877");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.UserId).ValueGeneratedNever();
+            entity.Property(e => e.DateOfBirth).HasColumnType("date");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+            entity.Property(e => e.PasswordSalt).HasMaxLength(255);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(40);
+            entity.Property(e => e.Username).HasMaxLength(25);
+            entity.Property(e => e.ZipCode).HasMaxLength(25);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Product__B40CC6CDAFC67BEA");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductId).ValueGeneratedNever();
+            entity.Property(e => e.Expiry).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(21, 4)");
+            entity.Property(e => e.Uom)
+                .HasMaxLength(20)
+                .HasColumnName("UOM");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
